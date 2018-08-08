b134a262c031bd5c77472eb8242eb1085c180662
suvratjain18 <jaintransport1996@gmail.com>
2018-08-08 13:57:39 +0530

    8 Aug Python Practical

---
+++ 8Aug.py
@@ -0,0 +1,27 @@
+A=10
+print(type(A))
+print(type(str(A)))
+print(A)
+print(type(A))
+
+easy_as = [1,2,3]
+print(easy_as)
+em = []
+print(em)
+letters = ['a,','b','c','d']
+print(letters)
+numbers=[2,3,5]
+print(numbers)
+ mixed =[4,5,"seconds"]
+ print(mixed)
+ numbers.append(7)
+print( numbers)
+numbers.append(11)
+print( numbers)
+ numbers.remove(11)
+print(numbers)
+print(numbers[-1])
+print(numbers[1:-1])
+
+x=[letters,numbers]
+print(x)
