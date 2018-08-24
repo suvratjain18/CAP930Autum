075d48bfcac1974e5968b8f1bd19b8e70f88965e
suvratjain18 <jaintransport1996@gmail.com>
2018-08-24 14:09:10 +0530

    CA1 File

---
+++ ca1file.py
@@ -0,0 +1,21 @@
+# We take input the first emp details
+emp=['empno_1171','empname_abcd','hiredate_20-mar-2012','sal_40000','expert_Big Data Developer'],['empno_1172','empname_MSD','hiredate_26-apr-2016','sal_65000','expert_Web Developer']
+# We Take Input from the second emp details
+emp2={'empno_1173','empname_VK','hiredate_24-Apr-2017','sal_70000','expert_Data Analyst'}
+#We print the employes details
+print(end='\n')
+print(end='\n')
+print("We Print Employee Details")
+emp=[emp,emp2]
+print(emp)
+
+print(end='\n')
+print(end='\n')
+
+print(" AFter Using loop We print the employees details")
+
+for i  in emp:
+    print(end='\n')
+    print(i)
+
+    
